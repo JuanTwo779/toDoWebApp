e02286c547accfee952f4064faa1351d79b41772
juanm <juanmangubat2@gmail.com>
2023-07-15 01:19:40 +1000

    Bean Scopes - Singleton and Prototype

--- spring-framework-02/spring-framework/src/main/java/com/omega/springframework/examples/d1/LazyInitialisationLauncherApplication.java
+++ spring-framework-02/spring-framework/src/main/java/com/omega/springframework/examples/d1/LazyInitialisationLauncherApplication.java
@@ -1,12 +1,10 @@
-package com.omega.springframework.examples.d1.a0;
+package com.omega.springframework.examples.d1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 class ClassA {
