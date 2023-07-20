88027bdf03f91a915dc2289da63d0249fd19369e
juanm <juanmangubat2@gmail.com>
2023-07-20 23:54:34 +1000

    XML Configuration

---
+++ spring-framework-02/spring-framework/src/main/java/com/omega/springframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,28 @@
+package com.omega.springframework.examples.h1;
+
+import com.omega.springframework.game.GameRunner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+
+public class XmlConfigurationContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+            System.out.println(context.getBean("name"));
+
+            context.getBean(GameRunner.class).run();
+        }
+
+    }
+}
+
+
+
